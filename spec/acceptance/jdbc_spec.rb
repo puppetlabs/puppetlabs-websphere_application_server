4b98bee50495696dddf1b908eba85db36e891946
Greg Hardy <greg.hardy@puppetlabs.com>
2016-09-28 15:24:43 +0100

    (maint) Added test coverage to jdbc provider and data type

--- spec/acceptance/jdbc_spec.rb
+++ spec/acceptance/jdbc_spec.rb
@@ -6,7 +6,7 @@ describe 'jdbc layer is setup and working' do
     @agent = WebSphereHelper.get_ihs_server
     WebSphereInstance.install(@agent)
     WebSphereDmgr.install(@agent)
-    hostname = @agent.hostname
+    @hostname = @agent.hostname
 
     @manifest = <<-MANIFEST
     websphere_jdbc_provider { '#{JDBCProviderConstants.jdbc_provider}':
@@ -16,7 +16,7 @@ describe 'jdbc layer is setup and working' do
       user           => '#{JDBCProviderConstants.user}',
       scope          => '#{JDBCProviderConstants.scope}',
       cell           => '#{JDBCProviderConstants.cell}',
-      node_name      => '#{hostname}',
+      node_name      => '#{@hostname}',
       server         => '#{JDBCProviderConstants.server}',
       dbtype         => '#{JDBCProviderConstants.dbtype}',
       providertype   => '#{JDBCProviderConstants.providertype}',
@@ -32,7 +32,7 @@ describe 'jdbc layer is setup and working' do
       user                          => '#{JDBCProviderConstants.user}',
       scope                         => '#{JDBCProviderConstants.scope}',
       cell                          => '#{JDBCProviderConstants.cell}',
-      node_name                     => '#{hostname}',
+      node_name                     => '#{@hostname}',
       server                        => '#{JDBCProviderConstants.server}',
       jdbc_provider                 => '#{JDBCDatasourceConstants.jdbc_provider}',
       jndi_name                     => '#{JDBCDatasourceConstants.jndi_name}',
@@ -52,4 +52,13 @@ describe 'jdbc layer is setup and working' do
   end
 
   it_behaves_like 'an idempotent resource'
+
+  it 'should have installed the thin client datasource and provider' do
+    @ws_admin_result = on(@agent, "su - webadmin -c \"#{WebSphereConstants.ws_admin} -lang jython -c \\\"print AdminConfig.list('DataSource',AdminConfig.getid('/Cell:#{JDBCProviderConstants.cell}/Node:#{@hostname}/'))\\\"\"", :acceptable_exit_codes => [0,1,103])
+    results = @ws_admin_result.stdout.split( /\r?\n/ )
+    expect(results.size).to eq 3
+    expect(results[0]).to match(/^WASX.*:.*is: DeploymentManager/)
+    expect(results[1]).to match(/^\"#{JDBCDatasourceConstants.jdbc_provider}.*#{JDBCProviderConstants.cell}\/nodes\/#{@hostname}.*#DataSource_.*/)
+    expect(results[2]).to match(/^OTiSDataSource\(cells\/#{JDBCProviderConstants.cell}\/nodes\/#{@hostname}\|resources.xml#builtin_DataSource_.*/)
+  end
 end
