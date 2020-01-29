e43eaa1da4f4cf9dc5e79cfb2684a6936dbfb3a7
Vladislav Korehov <v.korehov01@ctco.lv>
2020-01-29 18:06:48 +0200

    adding proper exists check

--- lib/puppet/provider/websphere_jaas_data/wsadmin.rb
+++ lib/puppet/provider/websphere_jaas_data/wsadmin.rb
@@ -4,23 +4,28 @@ Puppet::Type.type(:websphere_jaas_data).provide(:wsadmin, parent: Puppet::Provid
   desc 'wsadmin provider for `websphere_jaas_data`'
 
   def scope(what)
+    file = "#{resource[:profile_base]}/#{resource[:dmgr_profile]}"
     case resource[:scope]
     when 'cell'
       mod_path = "Cell=#{resource[:cell]}"
       get      = "Cell:#{resource[:cell]}"
       path     = "cells/#{resource[:cell]}"
+      file << "/config/cells/#{resource[:cell]}/variables.xml"
     when 'server'
       mod_path = "Cell=#{resource[:cell]},Server=#{resource[:server]}"
       get      = "Cell:#{resource[:cell]}/Node:#{resource[:node_name]}/Server:#{resource[:server]}"
       path     = "cells/#{resource[:cell]}/nodes/#{resource[:node_name]}/servers/#{resource[:server]}"
+      file << "/config/cells/#{resource[:cell]}/nodes/#{resource[:node_name]}/servers/#{resource[:server]}/variables.xml"
     when 'cluster'
       mod_path = "Cluster=#{resource[:cluster]}"
       get      = "Cell:#{resource[:cell]}/ServerCluster:#{resource[:cluster]}"
       path     = "cells/#{resource[:cell]}/clusters/#{resource[:cluster]}"
+      file += "/config/cells/#{resource[:cell]}/clusters/#{resource[:cluster]}/variables.xml"
     when 'node'
       mod_path = "Node=#{resource[:node_name]}"
       get      = "Cell:#{resource[:cell]}/Node:#{resource[:node_name]}"
       path     = "cells/#{resource[:cell]}/nodes/#{resource[:node_name]}"
+      file << "/config/cells/#{resource[:cell]}/nodes/#{resource[:node_name]}/variables.xml"
     end
 
     case what
@@ -30,6 +35,8 @@ Puppet::Type.type(:websphere_jaas_data).provide(:wsadmin, parent: Puppet::Provid
       return get
     when 'path'
       return path
+    when 'file'
+      return file
     end
   end
 
@@ -54,15 +61,19 @@ Puppet::Type.type(:websphere_jaas_data).provide(:wsadmin, parent: Puppet::Provid
   end
 
   def exists?
-    cmd = <<-END.unindent
-    AdminConfig.list("JAASAuthData")
-    END
+    unless File.exist?(scope('file'))
+      return false
+    end
 
-    debug "Get JAAS Config Data #{cmd}"
-    result = wsadmin(file: cmd, user: resource[:user])
-    debug "Result: #{result}"
+    debug "Retrieving value of #{resource[:name]} from #{scope('file')}"
+    doc = REXML::Document.new(File.open(scope('file')))
 
-    false # always false for now
+    path = XPath.first(doc, "//authDataEntries[@alias='#{resource[:name]}']")
+    value = XPath.first(path, '@alias') if path
+
+    debug "Exists? #{resource[:name]} is: #{value}"
+
+    !value.nil?
   end
 
   def destroy
