7b107f111fd5c6868beec0b37c100b1ab0cf6e82
Vladislav Korehov <v.korehov01@ctco.lv>
2020-01-29 09:40:13 +0200

    remove test content

--- manifests/profile/appserver.pp
+++ manifests/profile/appserver.pp
@@ -140,13 +140,6 @@ define websphere_application_server::profile::appserver (
     before => Websphere_federate["${title}_${dmgr_host}_${cell}"],
   }
 
-  File  {
-    ensure => 'file',
-    path   => "${profile_base}/${profile_name}/dmgr_${_dmgr_host}_${_cell}.yaml",
-    before => Websphere_federate["${title}_${dmgr_host}_${cell}"],
-    content=> "test content"
-  }
-
   notify { 'APPSERVER after collect':
     message  => "File path ${profile_base}/${profile_name}/dmgr_${_dmgr_host}_${_cell}.yaml",
   }
