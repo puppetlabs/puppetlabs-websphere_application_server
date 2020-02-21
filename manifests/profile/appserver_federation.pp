4c9da4ebd282bc8c51ec8710cc5635e748a38111
Vladislav Korehov <v.korehov01@ctco.lv>
2020-02-21 20:16:29 +0200

    restporing export/collection

--- manifests/profile/appserver_federation.pp
+++ manifests/profile/appserver_federation.pp
@@ -85,10 +85,10 @@ define websphere_application_server::profile::appserver_federation (
   $_dmgr_host = downcase($dmgr_host)
   $_cell = downcase($cell)
 
-  file { "/etc/dmgr_${_dmgr_host}_${_cell}":
+  ## Collect the federation resource
+  File <<| title == "/etc/dmgr_${_dmgr_host}_${_cell}" |>> {
     path   => "${profile_base}/${profile_name}/dmgr_${_dmgr_host}_${_cell}.yaml",
     before => Websphere_federate["${title}_${dmgr_host}_${cell}"],
-    content => "dmgr_soap: ${dmgr_port}\ndmgr_fqdn: ${dmgr_host}\ndmgr_cell: ${cell}"
   }
 
   if $manage_federation {
