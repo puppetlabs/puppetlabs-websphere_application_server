08dccf109153cc18672d5f5ae427ae0063f10f92
sheena <sheena@puppet.com>
2021-03-02 16:00:12 +0000

    (maint) update test plans

--- plans/pe_server_setup.pp
+++ plans/pe_server_setup.pp
@@ -4,13 +4,6 @@ plan websphere_application_server::pe_server_setup(
   # install pe server
   run_task('provision::install_pe', $pe_server)
 
-  # install the module
-  run_command('puppet module install puppetlabs-websphere_application_server', $pe_server)
-
   # set the ui password
   run_command('puppet infra console_password --password=litmus', $pe_server)
-
-  # work around for pe's module structure, for the UI
-  run_command('ln -s /etc/puppetlabs/code/environments/production/modules/puppetlabs-websphere_application_server /opt/puppetlabs/puppet/modules', $pe_server)
-  run_command('ln -s /etc/puppetlabs/code/environments/production/modules/ruby_task_helper /opt/puppetlabs/puppet/modules', $pe_server)
 }
