d4155c0d31bb42c4a0ec3fe93b11f59969d0fffb
Bogdan Iamandei <bogdan.iamandei@gmail.com>
2022-12-19 11:49:44 +1100

    Fix ternary operator syntax

--- lib/puppet/provider/websphere_jdbc_provider/wsadmin.rb
+++ lib/puppet/provider/websphere_jdbc_provider/wsadmin.rb
@@ -96,8 +96,8 @@ Puppet::Type.type(:websphere_jdbc_provider).provide(:wsadmin, parent: Puppet::Pr
 
     # This is a bit stupid - but you can't pass an empty array as an argument to Jython
     # so we have to d this.
-    classpath = resource[:classpath].empty? '' : resource[:classpath].to_s
-    nativepath = resource[:nativepath].empty? '' : resource[:nativepath].to_s
+    classpath = resource[:classpath].empty? ? '' : resource[:classpath].to_s
+    nativepath = resource[:nativepath].empty? ? '' : resource[:nativepath].to_s
 
     # Put the rest of the resource attributes together 
     extra_attrs = []
